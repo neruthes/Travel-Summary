d6e99283fe0d73f8f8f88c312eb07ab2154f3bd5
Neruthes (a.k.a. J.N.) <i@neruthes.xyz>
2020-04-30 10:38:52 +0000

    Updated README

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Travel-Summary
 
+## Introduction
+
+This is un Nodejs utility to render travel summary plaintext into HTML, which can be used as the source for generating images to be printed ou to be saved in mobile devices.
+
+There is un online web service version as well: [github.com/neruthes/travel-summary-web](https://github.com/neruthes/travel-summary-web).
+
 ## How to Use
 
 ### Install
@@ -18,12 +24,15 @@ NERUTHES
 SAAZYPMNL
 +82 (55) 5555-5555
 
+date>>
+2019-01-01
+
 flight>>
 CZ1234
 2019-01-01
-SHA T2
+SHA/T2
 23:00
-CAN T3
+CAN/T3
 00:10
 +1
 
@@ -32,6 +41,9 @@ IntelContinental Guangzhou
 +86 (20) 8922-8888
 12345678
 No. 828, Yuejiang Middle Road, Haizhu District, Guangzhou, 510308
+
+date>>
+2019-01-03
 
 train>>
 G99
@@ -44,7 +56,7 @@ Shanghai
 
 Line 7 for flights and trains is the indicator of date change upon arrival (e.g. being overnight). May be omitted if not overnight.
 
-For flights specifically, line 8 is optional if you would like to note ticket number. But you have to specify `+0` in line 7 in order to make use of this feature.
+For flights specifically, line 8 is optional if you would like to note ticket number. But you have to specify `+0` in line 7 in order to use this feature.
 
 ### Generate
 
--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const svgIcons = {
     flight: `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" viewBox="0 0 24 24" fill="#0066EE" width="48px" height="48px"><g><rect fill="none" height="24" width="24"/></g><g><g><g><path d="M2.5,19h19v2h-19V19z M22.07,9.64c-0.21-0.8-1.04-1.28-1.84-1.06L14.92,10l-6.9-6.43L6.09,4.08l4.14,7.17l-4.97,1.33 l-1.97-1.54l-1.45,0.39l2.59,4.49c0,0,7.12-1.9,16.57-4.43C21.81,11.26,22.28,10.44,22.07,9.64z"/></g></g></g></svg>`,
     train: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#0066EE" width="48px" height="48px"><path d="M12 2c-4 0-8 .5-8 4v9.5C4 17.43 5.57 19 7.5 19L6 20.5v.5h2.23l2-2H14l2 2h2v-.5L16.5 19c1.93 0 3.5-1.57 3.5-3.5V6c0-3.5-3.58-4-8-4zM7.5 17c-.83 0-1.5-.67-1.5-1.5S6.67 14 7.5 14s1.5.67 1.5 1.5S8.33 17 7.5 17zm3.5-7H6V6h5v4zm2 0V6h5v4h-5zm3.5 7c-.83 0-1.5-.67-1.5-1.5s.67-1.5 1.5-1.5 1.5.67 1.5 1.5-.67 1.5-1.5 1.5z"/><path d="M0 0h24v24H0V0z" fill="none"/></svg>`,
     hotel: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#0066EE" width="48px" height="48px"><path d="M0 0h24v24H0z" fill="none"/><path d="M7 13c1.66 0 3-1.34 3-3S8.66 7 7 7s-3 1.34-3 3 1.34 3 3 3zm12-6h-8v7H3V5H1v15h2v-3h18v3h2v-9c0-2.21-1.79-4-4-4z"/></svg>`,
-    note: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#0066EE" width="48px" height="48px"><path d="M0 0h24v24H0z" fill="none"/><path d="M14 2H6c-1.1 0-1.99.9-1.99 2L4 20c0 1.1.89 2 1.99 2H18c1.1 0 2-.9 2-2V8l-6-6zm2 14h-3v3h-2v-3H8v-2h3v-3h2v3h3v2zm-3-7V3.5L18.5 9H13z"/></svg>`
+    note: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="#0066EE" width="48px" height="48px"><path d="M0 0h24v24H0z" fill="none"/><path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"/><path d="M0 0h24v24H0z" fill="none"/></svg>`
 };
 // --------------------------------------------------------
 
@@ -32,15 +32,12 @@ const rightPad = (arr, len, filler) => {
 const gen = {
     note: (argv) => {
         return `<div class="section2" data-type="note">
-            <div class="section2-header">📎</div>
+            <div class="section2-header">${svgIcons.note}</div>
             <div class="section2-content">
                 <div class="row">
-                    ${argv.slice(0,1)}
-                </div>
-                <div class="row">
                 ${
-                    argv.slice(1).map(line => {
-                        return `<p>${line.replace(/^#\&nbsp;(.+)$/, '<span class="big2">$1</span>')}</p>`
+                    argv.map(line => {
+                        return `<p>${line.replace(/^#\&nbsp;(.+)$/, '<span class="big1">$1</span>')}</p>`
                     }).join('')
                 }
                 </div>
@@ -48,11 +45,17 @@ const gen = {
         </div>`;
     },
 
+    date: (argv) => {
+        return `<div class="ff-monospace" data-type="date">
+            ${argv[0]}
+        </div>`;
+    },
+
     contact: (argv) => {
         return `<div class="section2 ff-sansserif" data-type="contact">
             ${
                 argv.map(line => {
-                    return `<p>${line.replace(/^#\&nbsp;(.+)$/, '<span class="big2">$1</span>')}</p>`
+                    return `<p>${line.replace(/^#\&nbsp;(.+)$/, '<span class="big1">$1</span>')}</p>`
                 }).join('')
             }
         </div>`;
@@ -64,7 +67,7 @@ const gen = {
             <div class="section2-content">
                 <div class="row padless ff-monospace">
                     <div class="col-1 big1">
-                        ${argv[3]}&nbsp;&nbsp;&nbsp;&nbsp;<span class="lig-arrow-right">-&gt;</span><span class="sup invisible">!</span>
+                        ${argv[3]}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="lig-arrow-right">-&gt;</span><span class="sup invisible">!</span>
                     </div><div class="col-2 big1">
                         ${argv[5]}<span class="sup">${((argv[6] === undefined || argv[6] === '+0') ? '' : argv[6])}</span>
                     </div>
@@ -98,7 +101,7 @@ const gen = {
             <div class="section2-content">
                 <div class="row padless ff-monospace">
                     <div class="col-1 big1">
-                        ${argv[3]}&nbsp;&nbsp;&nbsp;&nbsp;<span class="lig-arrow-right">-&gt;</span><span class="sup invisible">!</span>
+                        ${argv[3]}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span class="lig-arrow-right">-&gt;</span><span class="sup invisible">!</span>
                     </div><div class="col-2 big1">
                         ${argv[5]}<span class="sup">${((argv[6] === undefined || argv[6] === '+0') ? '' : argv[6])}</span>
                     </div>
@@ -131,7 +134,7 @@ const gen = {
             <div class="section2-header">${svgIcons.hotel}</div>
             <div class="section2-content">
                 <div class="row">
-                    <div class="col big2 ff-sansserif">
+                    <div class="col big1 ff-sansserif">
                         ${argv[0]}
                     </div>
                 </div>
